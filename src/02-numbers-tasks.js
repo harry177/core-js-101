cf4319c73b96ed50de21ffb3bfb857b25d501933
Harry177 <harry177@rambler.ru>
2022-11-30 00:49:40 +0100

    feat: completed 02-numbers task and solved 13 first tasks from 03-arrays task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -201,8 +201,13 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -220,8 +225,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (Number(value)) {
+    return (Number(value));
+  }
+  return def;
 }
 
 module.exports = {
